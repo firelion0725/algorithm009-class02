a1a5a8375421f97165e4bab8b525c02f14aff648
Leo.ZhangTJ <Leo.ZhangTJ@homecreditcfc.cn>
2020-05-21 10:15:51 +0800

    合并有序数组

--- Week_01/MergeTwoSortedLists.java
+++ Week_01/MergeTwoSortedLists.java
@@ -9,7 +9,34 @@ class MergeTwoSortedLists {
     }
 
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        helper(head, l1, l2);
+        return head.next;
+    }
 
+    private void helper(ListNode result, ListNode l1, ListNode l2) {
+        if (result == null) {
+            return;
+        }
+
+        if (l1 == null) {
+            result.next = l2;
+            return;
+        }
+
+        if (l2 == null) {
+            result.next = l1;
+            return;
+        }
+
+        if (l1.val < l2.val) {
+            result.next = l1;
+            l1 = l1.next;
+        } else {
+            result.next = l2;
+            l2 = l2.next;
+        }
+        helper(result.next, l1, l2);
     }
 
 
