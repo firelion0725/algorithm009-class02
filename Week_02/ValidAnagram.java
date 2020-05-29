8cf4f83bbfdcb3cdef209bdef34bee538f932bec
Leo.ZhangTJ <Leo.ZhangTJ@homecreditcfc.cn>
2020-05-29 14:40:06 +0800

    作业

---
+++ Week_02/ValidAnagram.java
@@ -0,0 +1,16 @@
+/**
+ * https://leetcode-cn.com/problems/valid-anagram/description/
+ */
+class ValidAnagram {
+    public boolean isAnagram(String s, String t) {
+        char[] c1 = s.toCharArray();
+        char[] c2 = t.toCharArray();
+
+        Arrays.sort(c1);
+        Arrays.sort(c2);
+
+        String s1 = new String(c1);
+        String s2 = new String(c2);
+        return s1.equals(s2);
+    }
+}
